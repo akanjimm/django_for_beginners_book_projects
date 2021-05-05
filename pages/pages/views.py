67d22111d75caff516b9183cb8ad153bf1547741
akanjimm <akanjimm@gmail.com>
2021-05-05 08:42:04 +0100

    first commit

---
+++ pages/pages/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+class AboutTemplateView(TemplateView):
+    template_name = 'about.html'
